22e58c9117342cb3b0a15283b2f42f119a297fbc
Mayank Mishra <mishraprince0786@gmail.com>
2024-03-18 23:31:33 +0530

    Time: 57 ms (35.08%) | Memory: 69.1 MB (36.85%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.java
@@ -4,12 +4,10 @@ class Solution {
         int[] curr = points[0];
         int count = 1;
         for(int i = 1; i < points.length; i++){
-            // System.out.println(curr[0] + " " + curr[1]);
             if(curr[0] <= points[i][0] && points[i][0] <= curr[1]){
                 curr[0] = Math.max(curr[0], points[i][0]);
                 curr[1] = Math.min(curr[1], points[i][1]);
             }else{
-                // System.out.println(points[i][0] + " " + points[i][1]);
                 curr = points[i];
                 count++;
             }
