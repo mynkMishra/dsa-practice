68cf480f6b2659ebd18798e39b146fd2614beb70
Mayank Mishra <mishraprince0786@gmail.com>
2024-06-02 09:50:43 +0530

    Time: 515 ms (5.04%) | Memory: 88 MB (74.75%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,28 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let memo = {}
+    return function(...args) {
+        let key = args.join(",");
+        console.log(key);
+        if(memo[key] === undefined){
+             memo[key] = fn(...args);
+        }
+       
+        return memo[key]
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
