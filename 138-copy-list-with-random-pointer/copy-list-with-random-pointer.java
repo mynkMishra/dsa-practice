8368f3636637c4f688a448b3a4ebe9aff1e7a8ca
Mayank Mishra <mishraprince0786@gmail.com>
2024-06-21 23:18:08 +0530

    Time: 1 ms (9.71%) | Memory: 44.1 MB (85.99%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -15,33 +15,48 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Map<Node, Node> map = new HashMap<Node, Node>();
-
-        Node newHead = null;
-        Node temp = head;
-        Node newTemp = null;
-        while(temp != null){
-            if(newHead == null){
-                newHead = new Node(temp.val);
-                newTemp = newHead;
-                map.put(temp, newHead);
-                temp = temp.next;
-            }else{
-                newTemp.next = new Node(temp.val);
-                newTemp = newTemp.next;
-                map.put(temp, newTemp);
-                temp = temp.next;
-            }
+        
+        if(head == null){
+            return head;
         }
 
-        temp = head;
-        newTemp = newHead;
-        while(temp != null){
-            newTemp.random = map.get(temp.random);
-            temp = temp.next;
-            newTemp = newTemp.next;
+        Map<Node, Integer> map = new HashMap<Node, Integer>();
+
+        Node curr = head;
+
+        int idx = 0;
+        while(curr != null){
+            map.put(curr, idx++);
+            curr = curr.next;
+        }
+
+        curr = head;
+
+        Node newHead = new Node(head.val);
+        Node newCurr = newHead;
+        curr = curr.next;
+
+        idx = 0;
+        Map<Integer, Node> newMap = new HashMap<Integer, Node>();
+        newMap.put(idx++, newHead);
+
+        while(curr != null){
+            newCurr.next = new Node(curr.val);
+            newMap.put(idx++, newCurr.next);
+            curr = curr.next;
+            newCurr = newCurr.next;
+        }
+
+        curr = head;
+        newCurr = newHead;
+
+        while(curr != null){
+            newCurr.random = newMap.get(map.get(curr.random));
+            curr = curr.next;
+            newCurr = newCurr.next;
         }
 
         return newHead;
+
     }
 }
