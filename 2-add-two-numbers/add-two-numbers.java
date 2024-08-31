356d74a2ff494d631a0e763ea3c953adc5e3091a
Mayank Mishra <mishraprince0786@gmail.com>
2024-08-31 21:27:28 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (39.67%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -11,46 +11,76 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        ListNode l3 = null;
-        ListNode temp = null;
-        int carry = 0;
+        // l1 = reverse(l1);
+        // l2 = reverse(l2);
 
+        ListNode res = null;
+        ListNode newHead = null;
+
+        int carry = 0;
         while(l1 != null && l2 != null){
             int val = l1.val + l2.val + carry;
-            carry = val > 9 ? 1 : 0;
-
-            if(l3 == null){                
-                l3 = new ListNode(val % 10);
-                temp = l3;
+            if(val > 9){
+                carry = 1;
             }else{
-                temp.next = new ListNode(val % 10);
-                temp = temp.next;
+                carry = 0;
             }
 
+            if(res == null){
+                res = new ListNode(val%10);
+                newHead = res;
+            }else{
+                res.next = new ListNode(val%10);
+                res = res.next;
+            }
             l1 = l1.next;
             l2 = l2.next;
         }
 
         while(l1 != null){
             int val = l1.val + carry;
-            carry = val > 9 ? 1 : 0;
-            temp.next = new ListNode(val % 10);
-            temp = temp.next;
+            if(val > 9){
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+            res.next = new ListNode(val%10);
+            res = res.next;
             l1 = l1.next;
         }
 
         while(l2 != null){
             int val = l2.val + carry;
-            carry = val > 9 ? 1 : 0;
-            temp.next = new ListNode(val % 10);
-            temp = temp.next;
+            if(val > 9){
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+            res.next = new ListNode(val%10);
+            res = res.next;
             l2 = l2.next;
         }
 
         if(carry == 1){
-            temp.next = new ListNode(carry);
+            res.next = new ListNode(1);
         }
 
-        return l3;
+        return newHead;
+    }
+
+    public ListNode reverse(ListNode node){
+
+        ListNode prev = null;
+        ListNode curr = node;
+        ListNode next = node;
+
+        while(next != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
     }
 }
