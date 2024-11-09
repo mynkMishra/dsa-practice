3f7d777e9bb1e3c29eb153d69114929f46585695
Mayank Mishra <mishraprince0786@gmail.com>
2024-11-09 15:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (40.58%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    int[] ways;
+    public int climbStairs(int n) {
+
+        if(n == 1){
+            return 1;
+        }
+
+        ways = new int[n + 1];
+
+        ways[0] = 0;
+        ways[1] = 1;
+        ways[2] = 2;
+
+        for(int i = 3; i <= n; i++){
+            ways[i] = ways[i - 1] + ways[i - 2];
+        }
+
+        return ways[n];
+    }
+}
