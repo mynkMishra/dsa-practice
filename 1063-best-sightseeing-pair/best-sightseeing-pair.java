2bde172f155ac94637684dc8cbe2e6911282cd73
Mayank Mishra <mishraprince0786@gmail.com>
2024-12-27 11:43:18 +0530

    Time: 3 ms (97.03%) | Memory: 53.6 MB (41.35%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.java
@@ -3,24 +3,17 @@ class Solution {
         
         int N = values.length;
         int score = Integer.MIN_VALUE;
-        ArrayList<int[]> stack = new ArrayList<int[]>();
+        int bestOption = Integer.MIN_VALUE;
+        int idx = -1;
 
         for(int i = N - 1; i >= 0; i--){
             int el = values[i];
 
-            for(int j = 0; j < stack.size(); j++){
-                int[] p = stack.get(j);
-                score = Math.max(score, el + p[0] + i - p[1]);
-            }
+            score = Math.max(score, el + i + bestOption);
 
-            if(stack.size() == 0){
-                stack.add(new int[]{el, i});
-            }else{
-                while(stack.size() > 0 && el >= stack.get(stack.size() - 1)[0]){
-                    stack.remove(stack.size() - 1);
-                }
-
-                stack.add(new int[]{el, i});
+            if(bestOption < el - i){
+                bestOption = el - i;
+                idx = i;
             }
         }
 
