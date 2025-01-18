807b462a4808235c2fa3d23a4774834654ddd04e
Mayank Mishra <mishraprince0786@gmail.com>
2025-01-18 16:09:51 +0530

    Time: 9 ms (13.99%) | Memory: 45.3 MB (9.26%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,125 +1,86 @@
 class Solution {
 
-    List<List<String>> board;
     List<List<String>> ans;
-    boolean[][] visited;
-    int N;
     public List<List<String>> solveNQueens(int n) {
-        N = n;
-        visited = new boolean[n][n];
-        board = new ArrayList<List<String>>();
         ans = new ArrayList<List<String>>();
 
-        for(int i = 1; i <= n; i++){
-            List<String> list = new ArrayList<String>();
-            for(int j = 1; j <= n; j++){
-                list.add(".");
-            }
-            board.add(list);
-        }
+        String[][] matrix = new String[n][n];
 
-        solve(n, 0); 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                matrix[i][j] = ".";
+            }
+        }
+        for(int j = 0; j < n; j++){
+            matrix[0][j] = "Q";
+            solve(1, n - 1, matrix);
+            matrix[0][j] = ".";
+        }
 
         return ans;
     }
 
-    public boolean isValidRow(int r){
-        for(String str: board.get(r)){
-            if(str.equals("Q")){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public boolean isValidCol(int c){
-        for(int i = 0; i < board.size(); i++){
-            if(board.get(i).get(c).equals("Q")){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public boolean isValidDiagonal(int r, int c){
-
-        int n = board.size();
-        int i = r - 1;
-        int j = c - 1;
-
-        while(i >= 0 && j >= 0){
-            if(board.get(i).get(j).equals("Q")){
-                return false;
-            }
-            i--;
-            j--;
-        }
-
-        i = r - 1;
-        j = c + 1;
-
-        while(i >= 0 && j < n){
-            if(board.get(i).get(j).equals("Q")){
-                return false;
-            }
-            i--;
-            j++;
-        }
-
-        i = r + 1;
-        j = c + 1;
-
-        while(i < n && j < n){
-            if(board.get(i).get(j).equals("Q")){
-                return false;
-            }
-            i++;
-            j++;
-        }
-
-        i = r + 1;
-        j = c - 1;
-
-        while(i < n && j >= 0){
-            if(board.get(i).get(j).equals("Q")){
-                return false;
-            }
-            i++;
-            j--;
-        }
-
-        return true;
-    }
-
-    public void solve(int n, int r){
-
+    public void solve(int r, int n, String matrix[][]){
         if(n == 0){
             List<String> list = new ArrayList<String>();
-            for(int i = 0; i < N; i++){
-                String str = "";
-                for(int j = 0; j < N; j++){
-                    str += board.get(i).get(j);
+
+            for(String[] row: matrix){
+                StringBuilder sb = new StringBuilder();
+                for(int j = 0; j < matrix.length; j++){
+                    sb.append(row[j]);
                 }
-                list.add(str);
+                list.add(sb.toString());
             }
+
             ans.add(list);
-            return;
+            return; 
         }
 
-        if(r >= N){
-            return;
-        }
-
-        
-        for(int c = 0; c < N; c++){
-            if(board.get(r).get(c).equals(".") && isValidCol(c) && isValidRow(r) && isValidDiagonal(r,c)){
-                board.get(r).set(c, "Q");
-                solve(n - 1, r + 1) ;
-                board.get(r).set(c, ".");
+        for(int j = 0; j < matrix.length; j++){
+            if(isValid(r, j, matrix)){
+                matrix[r][j] = "Q";
+                solve(r + 1, n - 1, matrix);
+                matrix[r][j] = ".";
             }
         }
-        
+    }
+
+    public boolean isValid(int r, int c, String[][] matrix){
+
+        for(int i = 0; i < matrix.length; i++){
+            if(matrix[i][c].equals("Q")){
+                return false;
+            }
+        }
+
+        for(int j = 0; j < matrix[0].length; j++){
+            if(matrix[r][j].equals("Q")){
+                return false;
+            }
+        }
+
+        int i = r;
+        int j = c;
+        while(i >= 0 && j < matrix[0].length){
+            if(matrix[i][j].equals("Q")){
+                return false;
+            }
+
+            i--;
+            j++;
+        }
+
+        i = r;
+        j = c;
+        while(i >= 0 && j >= 0){
+            if(matrix[i][j].equals("Q")){
+                return false;
+            }
+
+            i--;
+            j--;
+        }
+
+        return true;
     }
 }
