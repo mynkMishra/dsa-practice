e403aa2d48cc9ff692a9f37956d3b5334bd674d8
Mayank Mishra <mishraprince0786@gmail.com>
2025-02-08 14:22:07 +0530

    Time: 1 ms (98.89%) | Memory: 42.5 MB (70.36%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.java
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -1,42 +1,43 @@
 class Solution {
+
     List<String> ans;
     public List<String> restoreIpAddresses(String s) {
         ans = new ArrayList<String>();
-        solve(0, 3, new ArrayList<String>(), s);
+        solve(0, 3, new StringBuilder(s));
         return ans;
     }
 
-    public void solve(int idx, int k, ArrayList<String> list, String s){
+    public void solve(int idx, int rem, StringBuilder sb){
 
-        if(k == 0){
-            if(isValid(s.substring(idx, s.length()))){
-                list.add(s.substring(idx, s.length()));
-                StringBuilder sb = new StringBuilder();
-                for(int i = 0; i < list.size(); i++){
-                    sb.append(list.get(i));
-                    if(i != list.size() - 1){
-                        sb.append(".");
-                    }
-                }
+        if(rem == 0){
+            if(isValid(sb.toString().substring(idx, sb.length()))){
                 ans.add(sb.toString());
-                list.remove(list.size() - 1);
             }
             return;
         }
 
-        int i = idx;
-        while(i < s.length() && isValid(s.substring(idx, i + 1))){
-            list.add(s.substring(idx, i + 1));
-            solve(i + 1, k - 1, list, s);
-            list.remove(list.size() - 1);
-            i++;
+        for(int i = 0; i < 3 && idx + i < sb.length(); i++){
+            if(isValid(sb.toString().substring(idx, idx + i + 1))){
+                sb.insert(idx + i + 1, '.');
+                solve(idx + i + 2, rem - 1, sb);
+                sb.deleteCharAt(idx + i + 1);
+            }
         }
     }
 
-    public boolean isValid(String s){
-        if(s.length() == 0 || ( s.length() > 1 && s.charAt(0) == '0')){
+    public boolean isValid(String str){
+        if(str.length() == 0 || str.length() > 3){
             return false;
         }
-        return (long)0 <= Long.parseLong(s) && Long.parseLong(s) <= (long)255;
+
+        if(str.charAt(0) == '0' && str.length() > 1){
+            return false;
+        }
+
+        if(Integer.parseInt(str, 10) > 255){
+            return false;
+        }
+
+        return true;
     }
 }
