e5222a0d808d7798056d9c9a339efa77e3816b68
Mayank Mishra <mishraprince0786@gmail.com>
2025-03-29 15:47:56 +0530

    Time: 41 ms (12.96%) | Memory: 46.2 MB (24.80%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -1,25 +1,51 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-        int[] freq = new int[26];
-        int maxFreq = 0;
+        
+        Map<Character, Integer> hm = new HashMap<>();
+
         for(char ch: tasks){
-            freq[ch - 'A']++;
-            maxFreq = Math.max(freq[ch - 'A'], maxFreq);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
         }
 
-        int maxCount = 0;
-        for(int i = 0; i < 26; i++){
-            if(freq[i] == maxFreq){
-                maxCount++;
+        PriorityQueue<int[]> maxH = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+
+        for(Character ch: hm.keySet()){
+            maxH.add(new int[]{ch - 'A', hm.get(ch)});
+        }
+
+        int count = 0;
+        while(maxH.size() > 0){
+            List<int[]> l = new ArrayList<>();
+
+            int q = 0;
+            Set<Character> set = new HashSet<>();
+            while(q <= n){
+                if(!maxH.isEmpty()){
+                    int[] el = maxH.poll();
+
+                    if(!set.contains((char)(el[0] + 'A'))){
+                        set.add((char)(el[0] + 'A'));
+                        el[1]--;
+                    }
+
+                    if(el[1] != 0){
+                        l.add(el);
+                    }
+                }
+                
+                count++;
+                q++;
+                
+                if(l.size() == 0 && maxH.isEmpty()){
+                    break;
+                }
+            }
+
+            for(int[] e: l){
+                maxH.add(e);
             }
         }
 
-        int partCount = maxFreq - 1;
-        int partLength = n - (maxCount - 1);
-        int emptySlots = partCount * partLength;
-        int availableTasks = tasks.length - maxFreq * maxCount;
-        int idles = Math.max(0, emptySlots - availableTasks);
-        
-        return tasks.length + idles;
+        return count;
     }
 }
