d8326da80ee14027b95df96f73a555f92dbe7742
Mayank Mishra <mishraprince0786@gmail.com>
2025-04-27 16:28:58 +0530

    Time: 307 ms (54.26%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,8 +1,9 @@
--- Write your PostgreSQL query statement below
-SELECT(
-SELECT DISTINCT salary AS "SecondHighestSalary"
-FROM Employee
-ORDER BY salary DESC
-LIMIT 1
-OFFSET 1
-);
+# Write your MySQL query statement below
+
+WITH cte AS (
+    SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) as 'num'
+    FROM Employee
+)
+SELECT MAX(salary) AS SecondHighestSalary
+FROM cte
+WHERE num = 2
