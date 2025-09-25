4374588ea3ae7b26d14190827c9f212193ae745f
Mayank Mishra <mishraprince0786@gmail.com>
2025-09-26 00:14:06 +0530

    Time: 6 ms (22.88%) | Memory: 45.3 MB (84.47%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -6,11 +6,21 @@ class Solution {
         N = triangle.size();
         dp = new int[N + 1][N + 1];
 
-        for(int i = 0; i < dp[0].length; i++){
-            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        // for(int i = 0; i < dp[0].length; i++){
+        //     Arrays.fill(dp[i], Integer.MAX_VALUE);
+        // }
+
+        List<List<Integer>> A = triangle;
+
+        for(int i = N- 1; i >= 0; i--){
+            for(int j = 0; j < A.get(i).size(); j++){
+                dp[i][j] = Math.min(A.get(i).get(j) + dp[i + 1][j], A.get(i).get(j) + dp[i + 1][j + 1]);
+            }
         }
 
-        return run(triangle, 0, 0);
+        return dp[0][0];
+
+        // return run(triangle, 0, 0);
     }
 
     public int run(List<List<Integer>> A, int i, int j){
