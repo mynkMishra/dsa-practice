51a8dea9f5f11decf4bc6a2b7b7bbe8436fa05fe
Mayank Mishra <mishraprince0786@gmail.com>
2025-09-26 10:50:51 +0530

    Time: 241 ms (10.95%) | Memory: 43.9 MB (5.68%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        int N = nums.length;
+        HashSet<String> hs = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        int sum = 0;
+        for(int i = 0; i < N - 2; i++){
+            int k = i + 2;
+            for(int j = i + 1; j < N - 1 && nums[i] != 0; j++){
+                int idx = find(nums, nums[i] + nums[j], k, nums.length - 1);
+
+                sum += (idx - (j + 1));
+            }
+        }
+
+        return sum;
+    }
+
+    public int find(int[] arr, int target, int l, int r){
+
+        while(l <= r){
+            int mid = l + (r - l)/2;
+
+            if(arr[mid] >= target){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+}
