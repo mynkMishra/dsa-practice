83b1cbc5ed5b9a1b929a5781f428f7a93c536e3f
Mayank Mishra <mishraprince0786@gmail.com>
2025-11-01 23:21:52 +0530

    Time: 595 ms (73.53%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,15 +1,21 @@
 # Write your MySQL query statement below
 
-WITH con AS (
-    SELECT user_id, COUNT(user_id) AS num
-    FROM Confirmations
-    WHERE action = "confirmed"
-    GROUP BY user_id, action),
-tot AS (
-    SELECT user_id, COUNT(user_id) AS des
-    FROM Confirmations
-    GROUP BY user_id)
-SELECT s.user_id, ROUND(IFNULL((con.num/tot.des), 0), 2) AS confirmation_rate
-FROM Signups s
-LEFT JOIN con ON con.user_id = s.user_id
-LEFT JOIN tot ON tot.user_id = s.user_id;
+WITH con_tab AS (
+    SELECT con.user_id, COUNT(*) as con_count 
+    FROM Confirmations con
+    WHERE con.action = "confirmed"
+    GROUP BY con.user_id
+),
+total_tab AS (
+    SELECT con.user_id, COUNT(*) as total_count 
+    FROM Confirmations con
+    GROUP BY con.user_id
+)
+SELECT si.user_id, IFNULL(ROUND(con_tab.con_count/total_tab.total_count, 2), 0) AS confirmation_rate
+FROM Signups si
+LEFT JOIN con_tab ON con_tab.user_id = si.user_id
+LEFT JOIN total_tab ON total_tab.user_id = si.user_id
+
+
+
+
