63aa4d4a4761e46b352361ddfbb8d6c4a695c720
Mayank Mishra <mishraprince0786@gmail.com>
2025-11-02 15:05:45 +0530

    Time: 548 ms (88.73%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
 
 
-WITH del_rank AS (
-    SELECT *, RANK() OVER(PARTITION BY customer_id ORDER BY order_date ASC) AS 'rank'
+WITH rank_tab AS (
+    SELECT *, 
+    RANK() OVER (PARTITION BY customer_id ORDER BY order_date ASC) AS 'ran'
     FROM Delivery
 )
-SELECT ROUND(SUM(dr.order_date = dr.customer_pref_delivery_date)*100/COUNT(*), 2) AS immediate_percentage
-FROM del_rank dr
-WHERE dr.rank = 1
+SELECT ROUND(SUM(rn.order_date = rn.customer_pref_delivery_date)*100/COUNT(*), 2) AS immediate_percentage
+FROM rank_tab rn
+WHERE rn.ran = 1
