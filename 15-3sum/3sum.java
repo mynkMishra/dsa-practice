a9bc4c2ed480e8c717cd19060c3c3964677edc5d
Mayank Mishra <mishraprince0786@gmail.com>
2026-01-01 14:10:48 +0530

    Time: 28 ms (95.20%) | Memory: 60.4 MB (8.06%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -2,42 +2,40 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
 
         for(int i = 0; i < nums.length; i++){
             if(i == 0 || nums[i - 1] != nums[i]){
-                int sum = -nums[i];
                 int l = i + 1;
                 int r = nums.length - 1;
 
-                while(l < r){
-                    if(nums[l] + nums[r] > sum){
-                        r--;
-                    }else if(nums[l] + nums[r] < sum){
-                        l++;
-                    }else{
+                int sum = -nums[i];
 
-                        while(l < r && nums[l] == nums[l + 1]){
+                while(l < r){
+                    if(nums[l] + nums[r] < sum){
+                        l++;
+                    }else if(nums[l] + nums[r] > sum){
+                        r--;
+                    }else{
+                        List<Integer> li = new ArrayList<>();
+                        li.add(nums[l]);
+                        li.add(nums[r]);
+                        li.add(nums[i]);
+
+                        ans.add(li);
+
+                        while(l < r && nums[l] + nums[r] == sum){
                             l++;
                         }
 
-                        while(l < r && nums[r - 1] == nums[r]){
+                        while(l < r && nums[l] + nums[r] == sum){
                             r--;
                         }
-                        List<Integer> li = new ArrayList<>();
-                        li.add(nums[i]);
-                        li.add(nums[l]);
-                        li.add(nums[r]);
-
-                        l++;
-                        r--;
-
-                        list.add(li);
                     }
                 }
             }
         }
 
-        return list;
+        return ans;
     }
 }
